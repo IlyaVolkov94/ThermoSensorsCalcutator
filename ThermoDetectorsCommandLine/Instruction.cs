f54feb3f905dbb7254c591a823f62553ec9da6a4
Ilya_Volkov <ilyasergeevichvolkov@gmail.com>
2023-02-05 18:05:42 +0300

    FirstCommit

---
+++ ThermoDetectorsCommandLine/Instruction.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Text;
+
+namespace ThermoDetectorsCommandLine;
+
+internal class Instruction
+{
+    readonly StringBuilder instruction = new();
+    internal string GetInstruction()
+    {
+        instruction.AppendLine("Привет, пользователь!");
+        instruction.AppendLine("Для расчета введите тип датчика, команду и значение:");
+        instruction.AppendLine("\tpt100 resistance 120,1");
+        instruction.AppendLine("\tk voltage 120,1 25");
+        instruction.AppendLine("Список команд:");
+        instruction.AppendLine("\tresistance - для расчета сопротивления");
+        instruction.AppendLine("\ttemperature - для расчета температуры");
+        instruction.AppendLine("\tvoltage - для расчета выходного напряжения термопары");
+        instruction.AppendLine("\tq - выход");
+        return instruction.ToString();
+    }
+}
--- ThermoDetectorsCommandLine/Program.cs
+++ ThermoDetectorsCommandLine/Program.cs
@@ -1,16 +1,14 @@
 ﻿// Console application for calculating temperature sensors
-using System.Text;
 using Automation;
+using ThermoDetectorsCommandLine;
 
-StringBuilder instruction = new();
-instruction.AppendLine("Привет, пользователь!");
-instruction.AppendLine("Для расчета введите тип датчика, команду и значение:");
-instruction.AppendLine("\tpt100 resistance 120,1");
-instruction.AppendLine("\tk voltage 120,1 25");
-instruction.AppendLine("Список команд:");
-instruction.AppendLine("\tresistance - для расчета сопротивления");
-instruction.AppendLine("\ttemperature - для расчета температуры");
-instruction.AppendLine("\tvoltage - для расчета выходного напряжения термопары");
-
-Console.WriteLine(instruction.ToString());
-var userString = Console.ReadLine();
+Instruction instruction = new();
+Console.WriteLine(instruction.GetInstruction());
+bool quitFlag = true;
+while (quitFlag)
+{
+    Console.Write("Ввод:\t");
+    var userString = Console.ReadLine()!.ToLower().Trim();
+    if (userString=="q")
+        quitFlag = false;
+}
